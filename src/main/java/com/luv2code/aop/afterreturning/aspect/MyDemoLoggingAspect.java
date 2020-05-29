bb42e701791ab9654775cd7ba4a42033e0f14806
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-05-29 22:10:07 +0200

    Added After Returning Advice demo app

--- src/main/java/com/luv2code/aop/afterreturning/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aop/afterreturning/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,8 @@ package com.luv2code.aop.afterreturning.aspect;
 
 import com.luv2code.aop.afterreturning.model.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -9,6 +11,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Aspect
 @Order(2)
@@ -33,6 +36,22 @@ public class MyDemoLoggingAspect {
                 System.out.println("accountName: " + ((Account) x).getName());
             }
         });
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* com.luv2code.aop.afterreturning.dao.AccountDao.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+
+        //wypisanie na rzecz jakiej metody uruchomiona zostałą porada
+        Signature signature = joinPoint.getSignature();
+        System.out.println("**** Advice method: Executing @AfterReturning advice on method: " + signature);
+
+        //wypisanie wyniku przechwyconej metody:
+        System.out.println("**** Advice method: Executing @AfterReturning advice; method result: " + result);
+
+
     }
 
 }
