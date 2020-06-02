d665a49c670115f7f7f1325c56545233b659aae0
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-06-02 20:59:43 +0200

    Added new package for Around advice type with exception handling demo app

---
+++ src/main/java/com/luv2code/aop/aroundwithhandleexception/service/TrafficFortuneService.java
@@ -0,0 +1,22 @@
+package com.luv2code.aop.aroundwithhandleexception.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class TrafficFortuneService {
+
+    public String getFortune() {
+
+        //simulate a delay
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //return a fortune
+        return "Expect heavy traffic this morning!";
+    }
+}
